a168d067c131dcbf9466acb9a324aa4d11b87578
TheLuis <luisdavidgilcabarcas47@gmail.com>
2026-04-08 11:32:45 -0500

    Desarrollo de las interfaces de produtos e info del producto

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import '../css/components/Main.css';
+
+import Add from '../assets/images/icon-plus.svg'
+import Subtract from '../assets/images/icon-minus.svg'
+import Cart from '../assets/images/icon-cart.svg';
+
 import img1 from "../assets/images/image-product-1.jpg";
 import img2 from "../assets/images/image-product-2.jpg";
 import img3 from "../assets/images/image-product-3.jpg";
@@ -37,7 +42,44 @@ const Main = () => {
                     </div>
                 </div>
                 <div className="container__info">
-
+                    <div className="info">
+                        <span>Sneaker company</span>
+                        <h2>Fall limited edition sneakers</h2>
+                        <p>
+                            These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.
+                        </p>
+                    </div>
+                    <div className="info__price">
+                        <div className="container__price">
+                            <span className="price">
+                                $125.00
+                            </span>
+                            <span className="percentage">
+                                50
+                                %
+                            </span>
+                        </div>
+                        <span className="discount">
+                            $250.00
+                        </span>
+                    </div>
+                    <div className="container__buy">
+                        <div className="amount">
+                            <button className="btn">
+                                <img src={Subtract} alt="Subtract" />
+                            </button>
+                            <span>
+                                0
+                            </span>
+                            <button className="btn">
+                                <img src={Add} alt="Add"/>
+                            </button>
+                        </div>
+                        <button className="buy">
+                            <img src={Cart} alt="Cart"/>
+                            Add to cart
+                        </button>
+                    </div>
                 </div>
             </section>
         </main>
